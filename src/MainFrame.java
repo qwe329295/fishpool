84d8a8d5e578516d5caf5529836e41946c9510e8
qwe329295 <105021028@live.asia.edu.tw>
2017-12-31 14:52:52 +0800

    1231

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,6 +1,5 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,9 +18,9 @@ public class MainFrame extends JFrame{
     private JLabel jlb =new JLabel("aa");
     private int imgW,imgH;
     private int submarineIndex=0;
-//    private ArrayList <Submarine> submarineList=new ArrayList<Submarine>();
+    private ArrayList <Submarine> submarineList=new ArrayList<Submarine>();
 //    private ArrayList <Submarine2> submarineList2=new ArrayList<Submarine2>();
-    private ArrayList <Thread> threadList=new ArrayList<Thread>();
+    private ArrayList <Thread> threadList =new ArrayList<Thread>();
 
     public MainFrame(){
         imgW=jpn.getImgWidth();
@@ -31,19 +30,26 @@ public class MainFrame extends JFrame{
         jpn.setLayout(null);
         toolPane.add(jbtnAddFish);
         toolPane.add(jbtnExit);
-        jlb.setBounds(100,100,80,30);
+//        jlb.setBounds(100,100,80,30);
 
         jbtnAddFish.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-//                submarineList.add(new Submarine(imgH,imgW));
-//                jpn.add(submarineList.get(submarineList.size()-1));
-//                threadList.add(new Thread(submarineList.get(submarineList.size()-1)));
+                submarineList.add(new Submarine(imgH,imgW));
+                jpn.add(submarineList.get(submarineList.size()-1));
+                threadList.add(new Thread(submarineList.get(submarineList.size()-1)));
                 threadList.get(threadList.size()-1).start();
             }
         });
 
+        jbtnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
         cp=this.getContentPane();
+        cp.setLayout(new BorderLayout(3,3));
         cp.add(jpn, BorderLayout.CENTER);
         cp.add(toolPane,BorderLayout.NORTH);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
---
+++ src/Submarine.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class Submarine extends JLabel implements Runnable {
+    private int frmW,frmH,x,y,r1,r;
+    private  ImageIcon[][] imgIcon={{new ImageIcon("1.png"),new ImageIcon("2.png"),
+    new ImageIcon("3.png")},{new ImageIcon("111.png"),new ImageIcon("222.png"),new ImageIcon("333.png")}};
+    private  boolean  dirFlag =true;
+    private Timer t1;
+    private Random rand=new Random();
+    public Submarine(int frmH, int frmW){
+        this.frmH=frmH;
+        this.frmW=frmW;
+        x=rand.nextInt(frmW-100);
+        y=rand.nextInt(frmH-100);
+        r=rand.nextInt(2);
+        if(r==1){
+            this.dirFlag=false;
+        }
+        this.setIcon(imgIcon[r][r1=rand.nextInt(5)]);
+        this.setBounds(x,y,this.getIcon().getIconWidth(),this.getIcon().getIconHeight());
+    }
+    @Override
+    public  void run(){
+        t1= new Timer(rand.nextInt(1000) + 50, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(Submarine.this.dirFlag){
+                    if((x-10)>0){
+                        x-=10;
+                    }else{
+                        Submarine.this.dirFlag=!Submarine.this.dirFlag;
+                        r=1;
+                        Submarine.this.setIcon(imgIcon[r][r1]);
+                    }
+                    Submarine.this.setLocation(x,y);
+                }else{
+                    if((x+ Submarine.this.getIcon().getIconWidth()+20<frmW)){
+                        x+=10;
+                    }else{
+                        Submarine.this.dirFlag=!Submarine.this.dirFlag;
+                        r=0;
+                        Submarine.this.setIcon((imgIcon[r][r1]));
+                        x-=10;
+                    }
+                    Submarine.this.setLocation(x,y);
+                }
+            }
+        });
+        t1.start();
+    }
+}
